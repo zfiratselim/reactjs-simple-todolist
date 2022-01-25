0dc9133be14c42f137874a086ad65766ada053ea
Fırat Selim ZİREKOĞLU <zirekoglufs@gmail.com>
2022-01-26 02:16:29 +0300

    finish todolist

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 
 function App() {
   const [todoList, setTodoList] = useState(["elma","armut"]);
+  const handleTodoList=newEl=>{
+   setTodoList(prev=>[newEl,...prev]);
+  }
   return (
     <div className="App">
-      <AddTodo/>
+      <AddTodo handleTodoList={handleTodoList}/>
       {
-        todoList.map((e, i)=>{
+        todoList.map((e, i) => {
           return <TodoListElement key={i} name={e}/>
         })
       }
@@ -15,19 +18,25 @@ function App() {
     </div>
   );
 }
+
 const AddTodo=props=>{
-  const [newTodo, setNewTodo]=useState("")
+  const [newTodo, setNewTodo] = useState("");
   return(
-   <div>
-     <input type="text" value={newTodo} onChange={e=>setNewTodo(e.target.value)} />
+   <div className="addTodoCon">
+     <input className="todotextbox" type="text" value={newTodo} onChange={e=>setNewTodo(e.target.value)} placeholder="add new todo!"/>
+     <input type="button" value="Add" onClick={()=>props.handleTodoList(newTodo)}/>
    </div>
   )
 }
 
-
 const TodoListElement=props=>{
+  const [checked,setChecked]= useState(false);
   return(
-    <h1>{props.name}</h1>
+    <div className="card">
+      <p className={checked?"line-through":""}>{props.name}</p>
+      <input type="checkbox"  onChange={e=>setChecked(!checked)} checked={checked}/>
+    </div>
   )
 }
+
 export default App;
